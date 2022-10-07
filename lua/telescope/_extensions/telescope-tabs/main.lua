2beb85708a2f79d95cd4469e080cc287d85dbbe8
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2022-10-07 13:54:54 +0200

    Improved preview

--- lua/telescope/_extensions/telescope-tabs/main.lua
+++ lua/telescope/_extensions/telescope-tabs/main.lua
@@ -2,7 +2,6 @@ local M = {}
 
 local pickers = require 'telescope.pickers'
 local finders = require 'telescope.finders'
-local previewers = require 'telescope.previewers'
 local actions = require 'telescope.actions'
 local action_state = require 'telescope.actions.state'
 local conf = require('telescope.config').values
@@ -11,6 +10,7 @@ M.list_tabs = function(opts)
 	local res = {}
 	for _, tid in ipairs(vim.api.nvim_list_tabpages()) do
 		local file_names = {}
+		local file_paths = {}
 		local file_ids = {}
 		local window_ids = {}
 		for _, wid in ipairs(vim.api.nvim_tabpage_list_wins(tid)) do
@@ -18,10 +18,11 @@ M.list_tabs = function(opts)
 			local path = vim.api.nvim_buf_get_name(bid)
 			local file_name = vim.fn.fnamemodify(path, ':t')
 			table.insert(file_names, file_name)
+			table.insert(file_paths, path)
 			table.insert(file_ids, bid)
 			table.insert(window_ids, wid)
 		end
-		table.insert(res, { file_names, file_ids, window_ids, tid })
+		table.insert(res, { file_names, file_paths, file_ids, window_ids, tid })
 	end
 	pickers
 		.new(opts, {
@@ -32,7 +33,8 @@ M.list_tabs = function(opts)
 					local entry_string = table.concat(entry[1], ', ')
 					return {
 						value = entry,
-						display = string.format('%d: %s', entry[4], entry_string),
+						path = entry[2][1],
+						display = string.format('%d: %s', entry[5], entry_string),
 						ordinal = entry_string,
 					}
 				end,
@@ -42,30 +44,24 @@ M.list_tabs = function(opts)
 				actions.select_default:replace(function()
 					actions.close(prompt_bufnr)
 					local selection = action_state.get_selected_entry()
-					vim.api.nvim_set_current_tabpage(selection.value[4])
+					vim.api.nvim_set_current_tabpage(selection.value[5])
 				end)
 				map('i', '<C-d>', function()
 					local current_picker = action_state.get_current_picker(prompt_bufnr)
 					local current_entry = action_state:get_selected_entry()
-					if vim.api.nvim_get_current_tabpage() == current_entry.value[4] then
+					if vim.api.nvim_get_current_tabpage() == current_entry.value[5] then
 						print 'You cannot close the currently visible tab :('
 						return
 					end
 					current_picker:delete_selection(function(selection)
-						for _, wid in ipairs(selection.value[3]) do
+						for _, wid in ipairs(selection.value[4]) do
 							vim.api.nvim_win_close(wid, false)
 						end
 					end)
 				end)
 				return true
 			end,
-			--TODO: use conf.file_previewer
-			previewer = previewers.new_buffer_previewer {
-				define_preview = function(self, entry)
-					local output = vim.api.nvim_buf_get_lines(entry.value[2][1], 0, -1, false)
-					vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, output)
-				end,
-			},
+			previewer = conf.file_previewer {},
 		})
 		:find()
 end
